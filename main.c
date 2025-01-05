be64eac1b78eda1cdf1aec8c7201470bf02648ed
joecus1 <joecusano57@gmail.comm>
2025-01-05 15:42:30 -0500

    Finished vm but it's buggy

--- main.c
+++ main.c
@@ -11,6 +11,8 @@
 
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];
+int running = 0;
+
 
 // Registers
 enum
@@ -28,6 +30,14 @@ enum
     R_COUNT
 };
 uint16_t reg[R_COUNT];
+
+
+// Memory Mapped Registers
+enum
+{
+    MR_KBSR = 0xFE00,
+    MR_KBDR = 0xFE02  
+};
 
 
 // Instructionos
@@ -52,7 +62,6 @@ enum
 };
 
 
-
 // Condition Flags
 enum
 {
@@ -60,6 +69,47 @@ enum
     FL_ZRO = 1 << 1,
     FL_NEG = 1 << 2,
 };
+
+
+// Trap Codes
+enum
+{
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
+};
+
+
+// Platform Specific to Linux
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
 
 
 uint16_t sign_extend(uint16_t x, int bit_count)
@@ -90,12 +140,61 @@ void update_flags(uint16_t r)
 
 uint16_t mem_read(uint16_t addr)
 {
-    return 0;
+    if (addr == MR_KBSR)
+    {
+        if (check_key())
+        {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        }   
+        else
+        {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[addr];
 }
 
 
-void mem_write(uint16_t addr, uint16_t obj)
+void mem_write(uint16_t addr, uint16_t val)
 {
+    memory[addr] = val;
+}
+
+
+uint16_t swap16(uint16_t x)
+{
+    return (x << 8) | (x >> 8);
+}
+
+
+void read_image_file(FILE* file)
+{
+    /* origin teslls where in memory to place image */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    /* read file */
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    while (read-- > 0)
+    {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+
+int read_image(const char* image_path)
+{
+    FILE* file = fopen(image_path, "rb");
+    if (!file) return 0;
+    read_image_file(file);
+    fclose(file);
+    return 1;
 }
 
 
@@ -249,7 +348,103 @@ void str(uint16_t instr)
     uint16_t r0 = (instr >> 9) & 0x7;
     uint16_t r1 = (instr >> 6) & 0x7;
     uint16_t offset = sign_extend(instr & 0x3F, 6);
-    mem_write(r1 + offset, r0);
+    mem_write(reg[r1] + offset, r0);
+}
+
+
+// Trap Functions
+void trap_puts()
+{
+    uint16_t* c = memory + reg[R_R0];
+    while (*c)
+    {
+        putc((char)*c , stdout);
+        ++c;
+    }
+    fflush(stdout);
+}
+
+
+void trap_getc()
+{
+    reg[R_R0] = (uint16_t)getchar();
+    update_flags(R_R0);
+}
+
+
+void trap_out()
+{
+    putc((char)reg[R_R0], stdout);
+    fflush(stdout);
+}
+
+
+void trap_in()
+{
+    printf("Enter a character: ");
+    char c = getchar();
+    putc(c, stdout);
+    fflush(stdout);
+    reg[R_R0] = (uint16_t)c;
+    update_flags(R_R0);
+}
+
+
+void trap_putsp()
+{
+    uint16_t* c = memory + reg[R_R0];
+    while (*c)
+    {
+        char char1 = (*c) & 0xFF;
+        putc(char1, stdout);
+        char char2 = (*c) >> 8;
+        if (char2) putc(char2, stdout);
+        ++c;
+    }
+    fflush(stdout);
+}
+
+
+void trap_halt()
+{
+    puts("HALT");
+    fflush(stdout);
+    running = 0;
+}
+
+
+void trap(uint16_t instr)
+{
+    reg[R_R7] = reg[R_PC];
+    switch (instr & 0xFF)
+    {
+        case TRAP_GETC:
+            trap_getc();
+            break;
+        case TRAP_OUT:
+            trap_out();
+            break;
+        case TRAP_PUTS:
+            trap_puts();
+            break;
+        case TRAP_IN:
+            trap_in();
+            break;
+        case TRAP_PUTSP:
+            trap_putsp();
+            break;
+        case TRAP_HALT:
+            trap_halt();
+            break;
+    }
+}
+
+
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
 }
 
 
@@ -262,15 +457,17 @@ int main(int argc, char* argv[])
         exit(2);
     }
 
-    // for (int i = 1; i < argc; ++i)
-    // {
-    //     if (!read_image(argv[i]))
-    //     {
-    //         printf("Failed to load image: %s\n", argv[i]);
-    //         exit(1);
-    //     }
+    for (int i = 1; i < argc; ++i)
+    {
+        if (!read_image(argv[i]))
+        {
+            printf("Failed to load image: %s\n", argv[i]);
+            exit(1);
+        }
 
-    // }
+    }
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
     
     
     // set z condition flag
@@ -280,12 +477,11 @@ int main(int argc, char* argv[])
     enum { PC_START = 0x3000 };
     reg[R_PC] = PC_START;
     
-    int running = 1;
+    running = 1;
     while (running)
     {
         // FETCH
-        // uint16_t instr = mem_read(reg[R_PC]++);
-        uint16_t instr = 30;
+        uint16_t instr = mem_read(reg[R_PC]++);
         uint16_t op = instr >> 12;
 
         switch (op)
@@ -311,16 +507,26 @@ int main(int argc, char* argv[])
                 ldr(instr);
                 break;
             case OP_STR:
+                str(instr);
+                break;
             case OP_NOT:
+                not(instr);
+                break;
             case OP_LDI:
                 ldi(instr);
                 break;
             case OP_STI:
+                sti(instr);
+                break;
             case OP_JMP:
+                jmp(instr);
+                break;
             case OP_LEA:
                 lea(instr);
                 break;
             case OP_TRAP:
+                trap(instr);
+                break;
             case OP_RES:
             case OP_RTI:
             default:
@@ -329,6 +535,7 @@ int main(int argc, char* argv[])
                 break;
         }
     }
+    restore_input_buffering();
 }
 
 
